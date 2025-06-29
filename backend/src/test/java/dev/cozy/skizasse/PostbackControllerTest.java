c2b2a429daa0456730c9e51db8380451d2818988
Cozy <25123254+ItsCosmas@users.noreply.github.com>
2025-06-29 09:19:40 +0300

    resolve formatting

--- backend/src/test/java/dev/cozy/skizasse/PostbackControllerTest.java
+++ backend/src/test/java/dev/cozy/skizasse/PostbackControllerTest.java
@@ -3,7 +3,6 @@ package dev.cozy.skizasse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
