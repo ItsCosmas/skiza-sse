caf1c25143cd465b1caf660dbba14874646303cf
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2025-12-11 11:27:35 +0300

    fix undefined url bug

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -2,11 +2,14 @@ import Nav from "components/nav/Nav";
 import Table from "components/table/Table";
 
 function Home() {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+  const postbackUrl = `${backendUrl}/api/postbacks`;
+
   return (
     <div className="container mx-auto">
       <Nav />
       <main className="container mx-auto mt-6">
-        <span className="flex justify-center mt-2">Send postbacks to {import.meta.env.VITE_BACKEND_URL+'/api/postbacks' || "http://localhost:8080/api/postbacks"}</span>
+        <span className="flex justify-center mt-2">Send postbacks to {postbackUrl}</span>
         <div className="divider"></div>
         <Table />
       </main>
